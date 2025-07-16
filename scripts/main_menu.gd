7ffc59baffb4220ef0c0c152c675d2419f2195ab
Debora Carvalho <dbndy3@gmail.com>
2025-07-16 00:15:51 -0300

    ✨feat: menu script

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,17 +1,45 @@
 extends Control
 
+signal change_scene(scene: String)
+
+@onready var menu_ost: AudioStreamPlayer2D = $"Menu ost"
+
+var is_transitioning: bool = false  # Controla se já está em transição
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
+	menu_ost.volume_db= -20
+	menu_ost.play()
+	
+	var tween := create_tween()
+	tween.tween_property(menu_ost, "volume_db", -5, 4.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 
-
-
 func _on_iniciar_pressed():
-	pass # Replace with function body.
+	# Evita múltiplos cliques durante a transição
+	if is_transitioning:
+		return
+	is_transitioning = true
+	
+	var fade_out_tween := create_tween()
+	fade_out_tween.tween_property(menu_ost, "volume_db", -0.0, 2.0) \
+		.set_trans(Tween.TRANS_SINE) \
+		.set_ease(Tween.EASE_IN_OUT)
+		
+	get_tree().quit()
+	
+func _on_sair_pressed() -> void:
+	# Fade out da música antes de sair
+	
+	var fade_out_tween := create_tween()
+	fade_out_tween.tween_property(menu_ost, "volume_db", -4.0, 1.0) \
+	.set_trans(Tween.TRANS_SINE) \
+	.set_ease(Tween.EASE_IN_OUT)
+	
+	await fade_out_tween.finished
+	get_tree().quit() 
+	
