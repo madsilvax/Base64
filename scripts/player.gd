65e6b37da1f18241e92967e521013c643b337650
Debora Carvalho <dbndy3@gmail.com>
2025-07-16 00:34:46 -0300

    ✨feat: simple player script

---
+++ scripts/player.gd
@@ -0,0 +1,57 @@
+extends CharacterBody2D
+class_name Player
+
+@export var speed = 100.0
+
+@onready var camera_2d = $camera
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $anim
+
+var can_interact = true
+var idle_direcao = ""
+var can_be_played = true
+
+signal interaction(dono)
+
+
+func _physics_process(delta: float) -> void:
+	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
+	
+	if direction and can_be_played:
+		velocity = direction * speed
+		if direction.x > 0: # Movendo direita
+			animated_sprite_2d.play("walk_right")
+			idle_direcao = "idle_right"
+		elif direction.x < 0: # Movendo esquerda
+			animated_sprite_2d.play("walk_left")
+			idle_direcao = "idle_left"
+		elif direction.y < 0: # Movendo para cima
+			animated_sprite_2d.play("walk_up")
+			idle_direcao = "idle_up"
+		elif direction.y > 0: # Movendo para baixo
+			animated_sprite_2d.play("walk_down")
+			idle_direcao = "idle_down"
+	else:
+		velocity = Vector2.ZERO
+		animated_sprite_2d.play(idle_direcao)
+	
+	move_and_slide()
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("interact") and can_interact and can_be_played:
+		interaction.emit(self)
+		print("emissao interaction")
+
+# Corpo Interagivel Entrou
+func _on_hurtbox_area_entered(area):
+	pass
+
+# Corpo Interagivel Saiu
+func _on_hurtbox_area_exited(area):
+	pass # Replace with function body.
+
+func _on_hurtbox_body_entered(body):
+	pass # Replace with function body.
+
+func _on_hurtbox_body_exited(body):
+	pass # Replace with function body.
