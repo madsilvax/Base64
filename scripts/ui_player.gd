f277f7af902cec07b4699a1c28d0cdf66f1b1663
matheus <madsilvax@gmail.com>
2025-07-19 23:49:24 -0300

    update pause

---
+++ scripts/ui_player.gd
@@ -0,0 +1,23 @@
+extends Control
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		$"UI layer/pause".show()
+		get_tree().paused = true
+		$"UI layer/pause/AnimationPlayer".play("blur")
+
+func _on_continue_pressed() -> void:
+	$"UI layer/pause".hide()
+	get_tree().paused = false
+	$"UI layer/pause/AnimationPlayer".play_backwards("blur")
+
+func _on_restart_pressed() -> void:
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_quit_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://screens/main_menu.tscn")
