b3435c89f80142fa074f1977fbbfd312e0f5deb0
Debora Carvalho <dbndy3@gmail.com>
2025-07-30 03:02:23 -0300

    everything

---
+++ scripts/kernel.gd
@@ -0,0 +1,94 @@
+extends CharacterBody2D
+
+@onready var shoot_point: Marker2D = $shoot_point
+
+@export var velocidade := 80.0
+@export var distancia_perseguir := 200.0
+@export var distancia_minima := 60.0
+@export var projectile_scene: PackedScene
+
+#vida
+@export var vida_maxima := 3  
+var vida_atual := vida_maxima
+
+@onready var anim = $AnimatedSprite2D
+@onready var shoot = $sfx_atirar
+@onready var hitbox = $hitbox
+
+var morto := false
+var player: Node2D = null
+var pode_atirar := true
+
+func _physics_process(delta):
+	if morto:
+		return  # Bloqueia tudo se já morreu
+	
+	if player == null:
+		anim.play("idle")
+		velocity = Vector2.ZERO
+		return
+
+	var direcao = player.global_position - global_position
+	var distancia = direcao.length()
+
+	if distancia > distancia_perseguir:
+		velocity = Vector2.ZERO
+		anim.play("idle")
+	elif distancia < distancia_minima:
+		velocity = Vector2.ZERO
+		anim.play("shoot")
+		atirar()
+	else:
+		direcao = direcao.normalized()
+		velocity = direcao * velocidade
+		move_and_slide()
+		anim.flip_h = direcao.x < 0
+		anim.play("run")
+
+func atirar():
+	if not pode_atirar or projectile_scene == null or player == null:
+		return
+
+	pode_atirar = false
+	shoot.play()
+
+	var proj = projectile_scene.instantiate()
+	get_tree().current_scene.add_child(proj)
+
+	proj.global_position = shoot_point.global_position
+	proj.initialize((player.global_position - shoot_point.global_position).normalized())
+
+	await get_tree().create_timer(0.8).timeout
+	pode_atirar = true
+
+#vida
+func receber_dano(valor):
+	if morto:
+		return
+	
+	vida_atual -= valor
+	print("Inimigo tomou dano, vida atual: ", vida_atual)
+	if vida_atual <= 0:
+		morrer()
+
+func morrer():
+	if morto:
+		return
+	morto = true
+	print("morri")
+	velocity = Vector2.ZERO
+	anim.play("death")
+	await get_tree().create_timer(2).timeout
+	queue_free()
+	
+func _on_hitbox_body_entered(body):
+	if body.is_in_group("projetil"):
+		morrer()
+
+func _on_player_detection_area_body_entered(body):
+	if body.is_in_group("player"):
+		player = body
+		atirar()
+func _on_player_detection_area_body_exited(body):
+	if body == player:
+		player = null
---
+++ scripts/projectile_cache.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+@export var speed := 600.0
+var direction: Vector2 = Vector2.ZERO
+
+func initialize(dir: Vector2) -> void:
+	direction = dir.normalized()
+	look_at(global_position + direction)
+
+func _ready():
+	$AnimatedSprite2D.play()
+
+func _physics_process(delta):
+	var collision = move_and_collide(direction * speed * delta)
+	if collision:
+		queue_free()
+	else:
+		position += direction * speed * delta
+
+func _on_life_timer_timeout():
+	queue_free()
+
+func _on_enemy_detection_area_body_entered(body):
+	if body.has_method("morrer"):
+		body.morrer()
